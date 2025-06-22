169a76a27e138a105a26b78b53d945921de63310
Paul <paulgulti20@gmail.com>
2025-06-22 19:01:38 +0300

    changed some projects and enhanced some ui aesthetics

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,13 +10,25 @@ import tailwindCss from '/tailwind-css.png';
 
 const About = () => {
   return (
-    <div id="about" className='container about'>
-      <p>Hi, I'm Paul</p>
-      <h2>FRONT-END <span>WEB DEVELOPER</span></h2>
+    <div id="about" className='container about inter'>
+      <h2>
+        <span>H</span>
+        <span>e</span>
+        <span>l</span>
+        <span>l</span>
+        <span>o</span>
+        <span>!</span>
+      </h2>
+      <div className='text-cont-cont'>
+        <div className='text-cont'>
+          <p className='text'>I am Paull</p>
+        </div>
+      </div>
+      <h3>FRONT-END <span>WEB DEVELOPER</span></h3>
       <p>A passionate Frontend web developer and UI/UX enthusiast specialized in building
         interactive and responsive websites.
       </p>
-      <h3>Skills</h3>
+      <h3 className='skills'>Skills</h3>
       <div className='skill-icons'>
         <div className='skill-icons-slide'>
           <div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,8 +4,8 @@ import Card from './Card'
 import shoppingCartImg from '/shoping-cart-1.png'
 import expenseTrackerImg from '/Expense-Tracker-1.png'
 import foodRecipeImg from '/food-recipe.png'
-import jsWeatherAppImg from '/js-weather-app.png'
 import moviePosterAppImg from '/movie-poster-4.png'
+import personalBlogAppImg from '/Personal-blog-app.png'
 
 const Projects = () => {
   return (
@@ -14,7 +14,7 @@ const Projects = () => {
       <Card title="E-commerce Storefront" stack="HTML, Tailwind, React" liveLink="https://e-commerce-storefront-pi.vercel.app/" sourceCodeLink="https://github.com/Paulgulti/E-commerce-Storefront" imgSrc={shoppingCartImg} />
       <Card title="Expense Tracker" stack="HTML, CSS, React" liveLink="https://paulgulti.github.io/ET/" sourceCodeLink="https://github.com/Paulgulti/ET" imgSrc={expenseTrackerImg} />
       <Card title="Recipe Finder" stack="HTML, CSS, React" liveLink="https://recipe-finder-eight-steel.vercel.app/" sourceCodeLink="https://github.com/Paulgulti/Recipe-finder" imgSrc={foodRecipeImg} />
-      <Card title="Weather App" stack="HTML, CSS, JS" liveLink="https://js-weather-app-fawn.vercel.app/" sourceCodeLink="https://github.com/Paulgulti/JS-Weather-App" imgSrc={jsWeatherAppImg} />
+      <Card title="Personal Blog App" stack="HTML, Next.js, Sanity, Tailwind, Shadcn" liveLink="https://personal-blog-nu-amber.vercel.app/" sourceCodeLink="https://github.com/Paulgulti/Personal_Blog" imgSrc={personalBlogAppImg} />
       <Card title="Movie Poster Finder" stack="HTML, Tailwind, React" liveLink="https://movie-poster-app-black.vercel.app/" sourceCodeLink="https://github.com/Paulgulti/Movie-Poster-App" imgSrc={moviePosterAppImg} />
     </div>
   )
